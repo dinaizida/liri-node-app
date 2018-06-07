6c6af76d6b8fda422528df1ad2f98fa3245b7cae
Dina <>
2018-06-07 16:55:38 -0500

    twitter added, tested with business name, working ok

--- liri.js
+++ liri.js
@@ -19,3 +19,55 @@ var keys = require("./keys");
 // be able to access your keys information like so
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+
+
+// declaring command variables-the name of a song, or movie
+var action = process.argv[2];
+var parameter = process.argv[3];
+
+
+// switch case for a command that the user enters
+switch (action) {
+	case "my-tweets":
+	twitter(parameter);
+	break;
+
+	case "spotify-this-song":
+	spotify(parameter);
+	break;
+
+	case "movie-this":
+	movie(parameter);
+	break;
+
+	case "do-what-it-says":
+	doit();
+	break;
+};
+
+function twitter(parameter){
+
+    var params = {screen_name: parameter, count: 20};
+	
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i ++){
+                console.log("Tweet: " + "'" + tweets[i].text + "'" + " Created At: " + tweets[i].created_at);
+            }
+        } else {
+            console.log(error);
+        }
+    });
+
+}
+
+function spotify(parameter){
+    
+}
+function movie(parameter){
+    
+}
+function doit(){
+    
+}

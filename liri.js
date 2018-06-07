8a5e243cc5d66f4fbf055b75b12107600ad18fb0
Dina <>
2018-06-07 15:03:14 -0500

    files added and liri.js started

---
+++ liri.js
@@ -0,0 +1,13 @@
+// npm packages import :
+// npm install  twitter --save
+// npm install  node-spotify-api --save
+// npm install  request --save
+// npm install  fs --save
+// npm install  dotenv --save
+
+
+var fs = require("fs");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var request = require("request")
+var keys = require("./keys");
